557454bac2b826475381acae57e451fd58b71ab4
MR.Wang <15268638853@163.com>
2019-08-27 22:29:04 +0800

    优化查询

--- todu/doAction.js
+++ todu/doAction.js
@@ -9,23 +9,27 @@ router.get('/',(req,res,next)=>{
     res.json({ user: '...' })
 })
 router.post('/getList',(req,res,next)=>{
-  
         let {page,row,key='',val=''}= req.body
         let sql  = `select * from todu `,
-            params = [];
+            count = `select count(*)as"total" from todu`
+            params = [],params1 = [],resuss='';
             if(key){
-                sql+='where ??=? '
+                sql+='where ??like? '
                 params.push(key)
                 params.push('%'+val+'%')
+                params1.push(key)
+                params1.push('%'+val+'%')
             }
+            sql+='order by createtime desc '
+           
             if(page && row){
                 sql+='limit ?,?';
                 params.push((page-1)*row)
                 params.push(+row)
             }
-            
-            connection(sql,params).then(result=>{
-                res.json({message:'true',data:result})
+            Promise.all([connection(sql+resuss,params),connection(count,params1)])
+            .then(result=>{
+                res.json({message:'true',data:result[0],total:result[1][0].total})
             }).catch(err=>{
                 next(err)
             })
