1e00098fab2bd727ccdb4043499205b5872ba3b1
wanghaibin <wanghb@lab.rhzz.com.cn>
2019-09-04 14:49:14 +0800

    优化代码

--- todu/doAction.js
+++ todu/doAction.js
@@ -5,37 +5,38 @@ const connection = require('../db');
 
 
 router.get('/',(req,res,next)=>{
-    console.log(req)
+   // console.log(req)
     res.json({ user: '...' })
 })
-router.post('/getList',(req,res,next)=>{
-        let {page,row,key='',val=''}= req.body
-        let sql  = `select * from todu `,
-            count = `select count(*)as"total" from todu `
-            params = [],params1 = [],resuss='';
-            if(key){
-                sql+='where ?? like ? '
-                count+='where ?? like ? '
-                params.push(key)
-                params.push('%'+val+'%')
-                params1.push(key)
-                params1.push('%'+val+'%')
-            }
-            sql+='order by createtime desc '
+router.post('/getList', (req,res,next) => {
+    let {page,row,key='',val=''}= req.body
+    let sql  = `select * from todu `,
+        count = `select count(*) as "total" from todu `
+        params = [],params1 = [],resuss='';
+    if(key){
+        count = sql+='where ?? like ? '
+        params.push(key)
+        params.push('%'+val+'%')
+        params1.push(key)
+        params1.push('%'+val+'%')
+    }
+    sql+='order by createtime desc '
            
-            if(page && row){
-                sql+='limit ?,?';
-                params.push((page-1)*row)
-                params.push(+row)
-            }
-            console.log(count)
-            Promise.all([connection(sql+resuss,params),connection(count,params1)])
-            .then(result=>{
-                console.log(result)
-                res.json({message:'true',data:result[0],total:result[1][0].total})
-            }).catch(err=>{
-                next(err)
-            })
+    if(page && row){
+        sql+='limit ?,?';
+        params.push((page-1)*row)
+        params.push(+row)
+    }
+   
+    Promise.all([
+        connection(sql+resuss,params),
+        connection(count,params1)
+    ]).then(result=>{
+       // console.log(result)
+        res.json({message:'true',data:result[0],total:result[1][0].total})
+    }).catch(err=>{
+        next(err)
+    })
         
 })
 router.post('/addList',(req,res,next)=>{
